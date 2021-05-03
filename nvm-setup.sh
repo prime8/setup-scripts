6c58b35ff86946a2ebd93b9b9416ada7f04b125e
Tim Bacon <timbacon+github@gmail.com>
2021-05-03 08:33:13 -0400

    Update nvm-setup.sh
    - change to `nvm-sh` repo
    - install last 2 `lts` node versions

--- nvm-setup.sh
+++ nvm-setup.sh
@@ -2,13 +2,13 @@
 
 set -e
 
-# See https://github.com/creationix/nvm
+# See https://github.com/nvm-sh/nvm
 
-LATEST_NVM=$(curl -Is https://github.com/creationix/nvm/releases/latest | grep 'Location:' | awk -F \/ '{print $NF}' | tr -d [:cntrl:])
+LATEST_NVM=$(curl -Is https://github.com/nvm-sh/nvm/releases/latest | grep -i 'location:' | awk -F \/ '{print $NF}' | tr -d \[:cntrl:\])
 CURRENT_NVM="$(grep '"version":' ~/.nvm/package.json | awk -F\" '{print "v"$(NF-1)}')"
 
 if [[ "$CURRENT_NVM" == "" || "$CURRENT_NVM" != "$LATEST_NVM" ]]; then
-  INSTALLER=https://raw.githubusercontent.com/creationix/nvm/$LATEST_NVM/install.sh
+  INSTALLER=https://raw.githubusercontent.com/nvm-sh/nvm/$LATEST_NVM/install.sh
   echo "Installing $INSTALLER"
   curl -s -o- $INSTALLER | bash
 else
@@ -19,7 +19,8 @@ source ~/.nvm/nvm.sh
 
 set +e
 
-for i in 8 10; do 
+RECENT_NODE_VERSIONS=$(nvm ls-remote --lts | awk -F '[v.]' '{print $(NF-2)}' | sort -rn | uniq | head -2)
+for i in $RECENT_NODE_VERSIONS; do 
   echo "*****************"
   echo "Setting up node $i"
   latest=$(nvm version-remote $i)

874e56744b0dea4ab3ef6edda7ce28d864d481e4
Tim Bacon <timbacon+github@gmail.com>
2021-05-03 08:39:36 -0400

    Update pull.sh
    - add auto-delete of "gone" branches (existing locally but not remotely)
    - add blank lines for clarity

--- pull.sh
+++ pull.sh
@@ -6,16 +6,22 @@ for d in $(find . -mindepth 1 -maxdepth 2 -type d -iname '.git' | sort); do
   if [ "$PULLDIR" == "." ]; then
     PULLDIR=$(pwd | xargs basename)
   fi
+
   echo -n "$(tput setaf 6)$PULLDIR$(tput sgr 0) "
+
   if ! (git diff-index --quiet HEAD); then
     git status --branch --short
   else
     DEFAULT_BRANCH=$(git branch -av | grep 'origin/HEAD' | awk '{print $3}' | cut -d/ -f2)
     BRANCH=$(test "$DEFAULT_BRANCH" != "" && echo $DEFAULT_BRANCH || echo master)
     git checkout $BRANCH --quiet && git fetch --quiet --all --prune && git rebase origin/$BRANCH --verbose
+
     for merged in $(git branch --merged | egrep -v "$BRANCH|master"); do
       git branch -d $merged
     done
+
+    git branch -av | grep '\[gone\]' | awk '{print $1}' | xargs -n 1 git branch -D
   fi
+
   popd 2>&1 >/dev/null
 done
